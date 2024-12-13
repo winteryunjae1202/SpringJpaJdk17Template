ab91788f7ceda6f7ec151380aa60393e78641adb
data8316-10 <winteryunjae1202@gmail.com>
2024-12-13 10:25:00 +0900

    코드 수정

--- src/main/java/kopo/poly/controller/OcrController.java
+++ src/main/java/kopo/poly/controller/OcrController.java
@@ -1,156 +1,128 @@
-//
-//import lombok.extern.slf4j.Slf4j;
-//import net.sourceforge.tess4j.Tesseract;
-//import net.sourceforge.tess4j.TesseractException;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
-//import org.springframework.web.multipart.MultipartFile;
-//
-//import java.io.*;
-//import java.net.URL;
-//import java.util.regex.Matcher;
-//import java.util.regex.Pattern;
-//
-//@Slf4j
-//@Controller
-//public class OcrController {
-//
-//    private static final String RELATIVE_PATH = "/uploads/";
-//
-//    @Value("${tess4j.datapath}")
-//    private String tessDataPath;
-//
-//    @Autowired
-//    private Tesseract tesseract;
-//
-//    @PostMapping("/api/upload")
-//    public ResponseEntity<String> uploadImage(@RequestParam("file") MultipartFile file, Model model){
-//
-//        log.info(this.getClass().getName() + ".uploadImage Start!");
-//
-//        String fourteenDigits = "";
-//
-//        try {
-//
-//            File convFile = downloadFile("https://orogno24buckets3.s3.ap-northeast-2.amazonaws.com/notices/orogno24_63_a122e623-7f44-4138-88c4-dce5618d14ec.jpg");
-////            file.transferTo(convFile);
-//
-//            // 이미지에서 텍스트 추출
-//            String extractedText = tesseract.doOCR(convFile);
-//
-//            // 정규 표현식을 사용하여 숫자 추출
-//            Pattern pattern = Pattern.compile("\\b\\d{12,15}\\b");
-//            Matcher matcher = pattern.matcher(extractedText);
-//
-//
-//            // 14자리의 연속된 숫자 찾기
-//            if (matcher.find()) {
-//                fourteenDigits = matcher.group();
-//                log.info("품목보고번호: " + fourteenDigits);
-//
-//            } else {
-//                log.info("품목보고번호를 인식하지 못했습니다.");
-//                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("품목보고번호를 인식하지 못했습니다.");
-//            }
-//
-//            // 파일 삭제
-////            if (convFile.delete()) {
-////                log.info("File deleted successfully: " + finalPath);
-////            } else {
-////                log.info("Failed to delete file: " + finalPath);
-////            }
-//
-//
-//        } catch (IOException | TesseractException e) {
-//            e.printStackTrace();
-//
-//            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("오류가 발생했습니다.");
-//        }
-//
-//        log.info(this.getClass().getName() + ".uploadImage End!");
-//
-//        return ResponseEntity.ok(fourteenDigits);
-//    }
-//
-//    private File downloadFile(String fileUrl) throws IOException {
-//        URL url = new URL(fileUrl);
-//        InputStream inputStream = url.openStream();
-//        String tempDir = System.getProperty("java.io.tmpdir");
-//        File tempFile = new File(tempDir + File.separator + "downloadedFile.jpg");
-//
-//        try (OutputStream outputStream = new FileOutputStream(tempFile)) {
-//            byte[] buffer = new byte[1024];
-//            int length;
-//            while ((length = inputStream.read(buffer)) != -1) {
-//                outputStream.write(buffer, 0, length);
-//            }
-//        }
-//
-//        return tempFile;
-//    }
-//}
-package kopo.poly.controller;//package kopo.poly.controller;
+package kopo.poly.controller;
 
-
-import kopo.poly.dto.OcrDTO;
-import kopo.poly.service.IOcrService;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.io.IOException;
 
-@RestController
-@RequestMapping("/api/card-info")
-@RequiredArgsConstructor
+import java.io.File;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 @Slf4j
+@Controller
 public class OcrController {
 
-    private final IOcrService ocrService;
+    @Autowired
+    private Tesseract tesseract;
 
-    @PostMapping(value = "/upload")
-    @ResponseBody
-    public ResponseEntity<String> upload(@RequestParam(value = "cardImage") MultipartFile mf, ModelMap model)
-            throws Exception {
+    @PostMapping("/api/upload")
+    public ResponseEntity<String> uploadImage(@RequestParam("file") MultipartFile file, Model model) {
 
-        OcrDTO rDTO = null;
+        log.info(this.getClass().getName() + ".uploadImage Start!");
+
+        String fourteenDigits = "";
+
         try {
+            // MultipartFile을 File 객체로 변환
+            File convFile = new File(System.getProperty("java.io.tmpdir") + "/" + file.getOriginalFilename());
+            file.transferTo(convFile);
 
-            log.info("받은 파일 : " + mf);
-            String fileSeq = "1";
+            // 이미지에서 텍스트 추출
+            String extractedText = tesseract.doOCR(convFile);
 
-            BufferedImage originalImage = loadImageFromUrl(mf);
-            log.info("BufferedImage : " + originalImage);
+            // 정규 표현식을 사용하여 숫자 추출
+            Pattern pattern = Pattern.compile("\\d{14}");
+            Matcher matcher = pattern.matcher(extractedText);
 
-//            BufferedImage highResolutionImage = resizeImageWithHigherResolution(originalImage, 1200,
-//                    1800); // 예시 해상도: 1200x1800
 
-            rDTO = ocrService.cardOcr(originalImage);
+            // 14자리의 연속된 숫자 찾기
+            if (matcher.find()) {
+                fourteenDigits = matcher.group();
+                log.info("14자리 연속된 숫자: " + fourteenDigits);
 
-        } catch (Exception e) {
+            } else {
+                log.info("품목보고번호를 인식하지 못했습니다.");
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("품목보고번호를 인식하지 못했습니다.");
+            }
+
+
+        } catch (IOException | TesseractException e) {
             e.printStackTrace();
+
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("오류가 발생했습니다.");
         }
 
-        return ResponseEntity.ok(rDTO.getPrdlstReportNo());
+        log.info(this.getClass().getName() + ".uploadImage End!");
 
+        return ResponseEntity.ok(fourteenDigits);
     }
-
-    public static BufferedImage loadImageFromUrl(MultipartFile image) throws IOException {
-        BufferedImage file = ImageIO.read(image.getInputStream());
-        return file;
-    }
-
-
-
 }
+//package kopo.poly.controller;//package kopo.poly.controller;
+//
+//
+//import kopo.poly.dto.OcrDTO;
+//import kopo.poly.service.IOcrService;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.ui.ModelMap;
+//import org.springframework.web.bind.annotation.*;
+//import org.springframework.web.multipart.MultipartFile;
+//
+//import javax.imageio.ImageIO;
+//import java.awt.image.BufferedImage;
+//import java.io.IOException;
+//
+//@RestController
+//@RequestMapping("/api/card-info")
+//@RequiredArgsConstructor
+//@Slf4j
+//public class OcrController {
+//
+//    private final IOcrService ocrService;
+//
+//    @PostMapping(value = "/upload")
+//    @ResponseBody
+//    public ResponseEntity<String> upload(@RequestParam(value = "cardImage") MultipartFile mf, ModelMap model)
+//            throws Exception {
+//
+//        OcrDTO rDTO = null;
+//        try {
+//
+//            log.info("받은 파일 : " + mf);
+//            String fileSeq = "1";
+//
+//            BufferedImage originalImage = loadImageFromUrl(mf);
+//            log.info("BufferedImage : " + originalImage);
+//
+////            BufferedImage highResolutionImage = resizeImageWithHigherResolution(originalImage, 1200,
+////                    1800); // 예시 해상도: 1200x1800
+//
+//            rDTO = ocrService.cardOcr(originalImage);
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//
+//        return ResponseEntity.ok(rDTO.getPrdlstReportNo());
+//
+//    }
+//
+//    public static BufferedImage loadImageFromUrl(MultipartFile image) throws IOException {
+//        BufferedImage file = ImageIO.read(image.getInputStream());
+//        return file;
+//    }
+//
+//
+//
+//}
--- src/main/java/kopo/poly/service/impl/OcrService.java
+++ src/main/java/kopo/poly/service/impl/OcrService.java
@@ -1,125 +1,127 @@
-package kopo.poly.service.impl;
 
 
-import kopo.poly.dto.OcrDTO;
-import kopo.poly.service.IOcrService;
-import kopo.poly.util.DateUtil;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.json.JSONArray;
-import org.json.JSONObject;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.*;
-import org.springframework.http.client.ClientHttpResponse;
-import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.util.Base64;
-
-@RequiredArgsConstructor
-@Service
-@Slf4j
-public class OcrService implements IOcrService {
-
-    @Value("${naver.service.secretKey}")
-    private String ocrSecretKey;
-
-    @Value("${naver.service.url}")
-    private String ocrInvokeUrl;
-
-    private final RestTemplate restTemplate;
-    /* 여기다간 네가 사용할 매퍼 적어주면 되고 */
-
-    /* URL에서 이미지를 다운로드하고 Base64로 인코딩하는 메소드 */
-    public static String encodeImageToBase64(BufferedImage image, String formatName) throws IOException {
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        ImageIO.write(image, formatName, outputStream);
-        return Base64.getEncoder().encodeToString(outputStream.toByteArray());
-    }
-
-    /* Naver Clova OCR 사용하여 영수증 스캔 */
-    @Override
-    public OcrDTO cardOcr(BufferedImage fileUrl) throws Exception {
-
-        // 파일 경로를 전달하여 이미지를 Base64로 인코딩
-        String base64Image = encodeImageToBase64(fileUrl, "png");
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("X-OCR-SECRET", ocrSecretKey); // OCR 비밀 키 추가
-        headers.setContentType(MediaType.APPLICATION_JSON); // 콘텐츠 타입을 JSON으로 설정
-
-        // JSON 객체 생성
-        JSONObject jsonObject = new JSONObject();
-        jsonObject.put("version", "V2"); // OCR API 버전 설정
-        jsonObject.put("requestId", "string"); // 요청 ID 설정
-        jsonObject.put("timestamp", 0); // 타임스탬프 설정
-
-        // 이미지 데이터를 위한 JSON 객체 생성
-        JSONObject data = new JSONObject();
-        data.put("format", "png"); // 이미지 형식 설정
-        data.put("data", base64Image); // Base64로 인코딩된 이미지 데이터 설정
-        data.put("name", "CardOcr" + DateUtil.getDateTime("HHmmss")); // 이미지 이름 설정 (시간 포함)
-
-        // JSON 배열에 이미지 데이터 추가
-        JSONArray img = new JSONArray();
-        img.put(data);
-        jsonObject.put("images", img);
-
-        log.info("Request Headers: " + headers); // 요청 헤더 로그 출력
-
-        // 요청 엔터티 생성 (JSON 본문과 헤더 포함)
-        HttpEntity<String> entity = new HttpEntity<>(jsonObject.toString(), headers);
-
-        // RestTemplate 인터셉터 추가
-        restTemplate.getInterceptors().add((request, body, execution) -> {
-            ClientHttpResponse response = execution.execute(request, body);
-            response.getHeaders().setContentType(MediaType.APPLICATION_JSON); // 응답의 콘텐츠 타입을 JSON으로 설정
-            return response;
-        });
-
-        // OCR API 호출
-        ResponseEntity<String> response = restTemplate.exchange(ocrInvokeUrl, HttpMethod.POST, entity, String.class);
-
-        // 응답 상태가 OK가 아니면 예외 발생
-        if (response.getStatusCode() != HttpStatus.OK) {
-            throw new RuntimeException("Failed to get OCR result: " + response.getStatusCode());
-        }
-
-        log.info("Response Body: " + response.getBody());
-
-        JSONObject responseBody = new JSONObject(response.getBody());
-        JSONArray responseImages = responseBody.getJSONArray("images");
-        JSONObject firstImage = responseImages.getJSONObject(0);
-        JSONArray fields = firstImage.getJSONArray("fields");
-
-        String prdlstReportNo = null;
-
-        // 필드 배열을 순회하면서 "inferText" 값이 "·품목보고번호:" 인 항목을 찾음
-        for (int i = 0; i < fields.length(); i++) {
-            JSONObject field = fields.getJSONObject(i);
-            String inferText = field.getString("inferText");
-            if ("·품목보고번호:".equals(inferText)) {
-                // 해당 항목의 다음 인덱스에 있는 값이 품목보고번호 값임
-
-                JSONObject nextField = fields.getJSONObject(i + 1);
-                prdlstReportNo = nextField.getString("inferText");
-                break;
-            }
-        }
-
-        log.info("품목보고번호: " + prdlstReportNo);
-
-
-        OcrDTO rDTO = OcrDTO.builder().prdlstReportNo(prdlstReportNo).build();
-        log.info("품목보고번호: " + rDTO);
-
-        log.info(this.getClass().getName() + "[service] Naver Clova OCR 사용하여 스캔 종료");
-
-        return rDTO;
-    }
-
-}
+//package kopo.poly.service.impl;
+//
+//
+//import kopo.poly.dto.OcrDTO;
+//import kopo.poly.service.IOcrService;
+//import kopo.poly.util.DateUtil;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.json.JSONArray;
+//import org.json.JSONObject;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.http.*;
+//import org.springframework.http.client.ClientHttpResponse;
+//import org.springframework.stereotype.Service;
+//import org.springframework.web.client.RestTemplate;
+//
+//import javax.imageio.ImageIO;
+//import java.awt.image.BufferedImage;
+//import java.io.ByteArrayOutputStream;
+//import java.io.IOException;
+//import java.util.Base64;
+//
+//@RequiredArgsConstructor
+//@Service
+//@Slf4j
+//public class OcrService implements IOcrService {
+//
+//    @Value("${naver.service.secretKey}")
+//    private String ocrSecretKey;
+//
+//    @Value("${naver.service.url}")
+//    private String ocrInvokeUrl;
+//
+//    private final RestTemplate restTemplate;
+//    /* 여기다간 네가 사용할 매퍼 적어주면 되고 */
+//
+//    /* URL에서 이미지를 다운로드하고 Base64로 인코딩하는 메소드 */
+//    public static String encodeImageToBase64(BufferedImage image, String formatName) throws IOException {
+//        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+//        ImageIO.write(image, formatName, outputStream);
+//        return Base64.getEncoder().encodeToString(outputStream.toByteArray());
+//    }
+//
+//    /* Naver Clova OCR 사용하여 영수증 스캔 */
+//    @Override
+//    public OcrDTO cardOcr(BufferedImage fileUrl) throws Exception {
+//
+//        // 파일 경로를 전달하여 이미지를 Base64로 인코딩
+//        String base64Image = encodeImageToBase64(fileUrl, "png");
+//
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.add("X-OCR-SECRET", ocrSecretKey); // OCR 비밀 키 추가
+//        headers.setContentType(MediaType.APPLICATION_JSON); // 콘텐츠 타입을 JSON으로 설정
+//
+//        // JSON 객체 생성
+//        JSONObject jsonObject = new JSONObject();
+//        jsonObject.put("version", "V2"); // OCR API 버전 설정
+//        jsonObject.put("requestId", "string"); // 요청 ID 설정
+//        jsonObject.put("timestamp", 0); // 타임스탬프 설정
+//
+//        // 이미지 데이터를 위한 JSON 객체 생성
+//        JSONObject data = new JSONObject();
+//        data.put("format", "png"); // 이미지 형식 설정
+//        data.put("data", base64Image); // Base64로 인코딩된 이미지 데이터 설정
+//        data.put("name", "CardOcr" + DateUtil.getDateTime("HHmmss")); // 이미지 이름 설정 (시간 포함)
+//
+//        // JSON 배열에 이미지 데이터 추가
+//        JSONArray img = new JSONArray();
+//        img.put(data);
+//        jsonObject.put("images", img);
+//
+//        log.info("Request Headers: " + headers); // 요청 헤더 로그 출력
+//
+//        // 요청 엔터티 생성 (JSON 본문과 헤더 포함)
+//        HttpEntity<String> entity = new HttpEntity<>(jsonObject.toString(), headers);
+//
+//        // RestTemplate 인터셉터 추가
+//        restTemplate.getInterceptors().add((request, body, execution) -> {
+//            ClientHttpResponse response = execution.execute(request, body);
+//            response.getHeaders().setContentType(MediaType.APPLICATION_JSON); // 응답의 콘텐츠 타입을 JSON으로 설정
+//            return response;
+//        });
+//
+//        // OCR API 호출
+//        ResponseEntity<String> response = restTemplate.exchange(ocrInvokeUrl, HttpMethod.POST, entity, String.class);
+//
+//        // 응답 상태가 OK가 아니면 예외 발생
+//        if (response.getStatusCode() != HttpStatus.OK) {
+//            throw new RuntimeException("Failed to get OCR result: " + response.getStatusCode());
+//        }
+//
+//        log.info("Response Body: " + response.getBody());
+//
+//        JSONObject responseBody = new JSONObject(response.getBody());
+//        JSONArray responseImages = responseBody.getJSONArray("images");
+//        JSONObject firstImage = responseImages.getJSONObject(0);
+//        JSONArray fields = firstImage.getJSONArray("fields");
+//
+//        String prdlstReportNo = null;
+//
+//        // 필드 배열을 순회하면서 "inferText" 값이 "·품목보고번호:" 인 항목을 찾음
+//        for (int i = 0; i < fields.length(); i++) {
+//            JSONObject field = fields.getJSONObject(i);
+//            String inferText = field.getString("inferText");
+//            if ("·품목보고번호:".equals(inferText)) {
+//                // 해당 항목의 다음 인덱스에 있는 값이 품목보고번호 값임
+//
+//                JSONObject nextField = fields.getJSONObject(i + 1);
+//                prdlstReportNo = nextField.getString("inferText");
+//                break;
+//            }
+//        }
+//
+//        log.info("품목보고번호: " + prdlstReportNo);
+//
+//
+//        OcrDTO rDTO = OcrDTO.builder().prdlstReportNo(prdlstReportNo).build();
+//        log.info("품목보고번호: " + rDTO);
+//
+//        log.info(this.getClass().getName() + "[service] Naver Clova OCR 사용하여 스캔 종료");
+//
+//        return rDTO;
+//    }
+//
+//}
